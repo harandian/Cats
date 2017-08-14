8dc8cfad5649cf2e4e81a11fc6ed2117330ae5c5
nekomeo <linh.tu545@gmail.com>
2017-08-14 11:20:21 -0700

    Initial commit

---
+++ Cats/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cats
+//
+//  Created by Elle Ti on 2017-08-14.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
