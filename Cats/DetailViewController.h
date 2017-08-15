da3823153a950d48c4a24b3c80e5b9c1f4319a28
harandian <hharandian@gmail.com>
2017-08-15 14:58:12 -0700

    first commit

---
+++ Cats/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Cats
+//
+//  Created by Hirad on 2017-08-15.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface DetailViewController : ViewController
+
+@end
